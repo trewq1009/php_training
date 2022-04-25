6cc71ce8000ecc767f5f070280a77badd21665f5
JeongKuk Kim <trewq1009@gmail.com>
2022-04-25 11:47:14 +0900

    trade update

--- view/trade/detail.php
+++ view/trade/detail.php
@@ -9,6 +9,9 @@ try {
     if(empty($_GET['boardNo'])) {
         throw new Exception('잘못된 경로 입니다.');
     }
+    if(empty($auth)) {
+        throw new Exception('로그인 후 이용 가능 합니다.');
+    }
     $db = new Database;
     $boardData = $db->findOne('tr_board', ['no'=>$_GET['boardNo']]);
     $productData = $db->findOne('tr_product', ['no'=>$boardData['reference_no']]);
